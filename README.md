49588665e9a4000646262d497c6dab42355a6436
iwata-motonori <motonori.iwata@dena.com>
2014-08-17 14:45:57 +0900

    generate scaffold by Minilla

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::Docker::Image - It's new $module
+
+# SYNOPSIS
+
+    use Test::Docker::Image;
+
+# DESCRIPTION
+
+Test::Docker::Image is ...
+
+# LICENSE
+
+Copyright (C) Motonori Iwata.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Motonori Iwata <gootonori+github@gmail.com>
---
+++ lib/Test/Docker/Image.pm
@@ -0,0 +1,39 @@
+package Test::Docker::Image;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Docker::Image - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Docker::Image;
+
+=head1 DESCRIPTION
+
+Test::Docker::Image is ...
+
+=head1 LICENSE
+
+Copyright (C) iwata-motonori.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Motonori Iwata E<lt>gootonroi+github@gmail.comE<gt>
+
+=cut
+
